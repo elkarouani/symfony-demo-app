de10e4c4a3d78b83efc40d50d52cb2419a21d39e
elkarouani <elkarouani@gmail.com>
2019-05-21 14:51:43 +0000

    Task: adding creating posts functionality

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -4,16 +4,36 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Post;
 
+/**
+ * @Route("/post", name="post.")
+ */
 class PostController extends AbstractController
 {
     /**
-     * @Route("/post", name="post")
+     * @Route("/", name="index")
      */
     public function index()
     {
         return $this->render('post/index.html.twig', [
             'controller_name' => 'PostController',
         ]);
+    }
+
+    /**
+     * @Route("/create", name="create")
+     * @param Request $request
+     * @return Response
+     */
+    public function create(Request $request) {
+        $post = new Post();
+        $post->setTitle('default title');
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($post);
+        $em->flush();
+        return new Response('Post was created');
     }
 }
