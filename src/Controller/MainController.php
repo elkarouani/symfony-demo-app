73322746474ff46bfa7c81715dc55ee6f286699c
elkarouani <elkarouani@gmail.com>
2019-05-21 13:16:30 +0000

    Task: adding random routes on main controller

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,17 +4,30 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 class MainController extends AbstractController
 {
     /**
-     * @Route("/main", name="main")
+     * @Route("/", name="home")
      */
     public function index()
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/MainController.php',
-        ]);
+        // return $this->json([
+        //     'message' => 'Welcome to your new controller!'
+        // ]);
+        return new Response('<h1>Welcome to your new controller!</h1>');
+    }
+
+    /**
+     * @Route("/custom/{name?}", name="custom")
+     * @param Request $request
+     * @return Response
+     */
+    public function custom(Request $request)
+    {
+        // dump($request->get('name'));
+        return new Response('<h1>Welcome ' . $request->get('name') . ' </h1>');
     }
 }
